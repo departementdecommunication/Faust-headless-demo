a0089582db8c305463102c83a20e44d7fd1d0064
Cecilia Martinez <cecilia.martinez@gmail.com>
2022-12-01 14:49:19 +0100

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../faust.config';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps /*, AppContext */ } from 'next/app';
+import { FaustProvider } from '@faustjs/next';
+import { client } from '../client';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <FaustProvider client={client} pageProps={pageProps}>
+      <Component {...pageProps} />
+    </FaustProvider>
+  );
 }
+
+export default MyApp;
